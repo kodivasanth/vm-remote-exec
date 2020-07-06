c56d756ca44fa1f53cfb2acd2766d204c9aee5f8
kodivasanth <kvasanthkumar@gmail.com>
2020-07-06 23:17:43 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,34 +1,4 @@
-# Create a new ssh key 
-resource "ibm_compute_ssh_key" "ssh_key" {
-  label      = "${var.ssh-label}-${random_id.val.hex}"
-  notes      = "for VM"
-  public_key = "${var.ssh-key}"
-}
-
-resource "ibm_compute_vm_instance" "vm_local_exec_sample" {
-    hostname = "${var.vm-hostname}"
-    domain = "${var.vm-domain}"
-    os_reference_code = "${var.vm-os-reference-code}"
-    datacenter = "${var.datacenter}"
-    network_speed = 10
-    hourly_billing = true
-    private_network_only = false
-    cores = 1
-    memory = "${var.vm-memory}"
-    disks = [25, 10, 20]
-    user_metadata = "{\"value\":\"newvalue\"}"
-    dedicated_acct_host_only = true
-    local_disk = false
-    ssh_key_ids = ["${ibm_compute_ssh_key.ssh_key.id}"]
-
-    /*
-    provisioner "file" {
-      source = "nginx.sh"
-      destination = "/tmp/nginx.sh"
-    }
-    */
-
-    provisioner "remote-exec" {
+ provisioner "remote-exec" {
       inline = [
         #"sh /tmp/nginx.sh"
         "yum install nginx -y"
